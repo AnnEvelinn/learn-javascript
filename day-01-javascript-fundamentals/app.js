36a52f9f0bac953a70910115df4d70544dfba23f
Ann Evelin <evelinvincentkattor@gmail.com>
2026-06-05 02:24:41 +0530

    Day 1: Introduction to JavaScript fundamentals

---
+++ day-01-javascript-fundamentals/app.js
@@ -0,0 +1,42 @@
+
+
+
+
+
+// This is a single-line comment
+
+/*
+This is a
+multi-line comment
+*/
+console.log("Introduction to JavaScript");
+
+console.log(25);
+console.log("Hello JavaScript");
+console.log(true);
+console.log(null);
+console.log(undefined);
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+
+console.log("Hello" + " " + "World");
+
+console.log("JavaScript".length);
+
+console.log("hello".toUpperCase());
+console.log("HELLO".toLowerCase());
+console.log("   JavaScript   ".trim());
+
+console.log(Math.PI);
+console.log(Math.random());
+console.log(Math.floor(7.9));
+console.log(Math.ceil(7.1));
+console.log(Math.round(7.4));
+console.log(Math.floor(Math.random()*51));
+
+
+
